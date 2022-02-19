0c75b368d526b4b1f0a5c113ad44c08acfa598b6
chaithumohan14 <kingchaithu14@gmail.com>
2022-02-19 17:58:45 +0530

    CORS error 2

--- src/app.mjs
+++ src/app.mjs
@@ -4,19 +4,19 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3000/",
-      "https://www.datator.tech",
-      "https://www.datator.tech/",
-      "https://www.data-tor.com",
-      "https://www.data-tor.com/",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "http://localhost:3000/",
+//       "https://www.datator.tech",
+//       "https://www.datator.tech/",
+//       "https://www.data-tor.com",
+//       "https://www.data-tor.com/",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 
 app.post("/register", (req, res) => {
