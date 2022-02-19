526e3a57f5acf0466096b2067cd3095d8fbfcf03
chaithumohan14 <kingchaithu14@gmail.com>
2022-02-19 18:14:54 +0530

    IATW add login endpoint

--- src/app.mjs
+++ src/app.mjs
@@ -19,6 +19,8 @@ dotenv.config();
 // );
 app.use(express.json());
 
+app.post("/login", (_, res) => res.status(400).json({ msg: "User not found" }));
+
 app.post("/register", (req, res) => {
   console.log(
     "NEW USER: " + "EMAIL -- " + req.body.email + "\t NAME -- " + req.body.name
